f646e8e4e8e120a3327a4b6e1b132a3c971e5162
Bekir-shaher <aldubaiabubaker@gmail.com>
2025-08-14 09:22:16 +0300

    add project structure and splash screen

---
+++ lib/futuers/user/presention/screens/firstScreen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class Firstscreen extends StatefulWidget {
+  const Firstscreen({super.key});
+
+  @override
+  State<Firstscreen> createState() => _FirstscreenState();
+}
+
+class _FirstscreenState extends State<Firstscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffE9FAFF),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Center(
+            child: Container(
+              width: 276,
+              height: 112,
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('images/firstScreenAssets/logo.png'),
+            ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(top: 120),
+        padding: EdgeInsets.only(bottom: 44),
+        width: 245,
+        height: 374,
+        child: Stack(
+          alignment: Alignment.center, // Center the stack
+          children: [
+            Image.asset('images/firstScreenAssets/build.png'),
+            Positioned(
+              top: 120,
+              child: Image.asset(
+                'images/firstScreenAssets/motor.png',
+                height: 209,
+                width: 270,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+Stack(
+  alignment: Alignment.center, // محاذاة افتراضية لكل العناصر
+  children: [
+    // العنصر الأول (في الخلفية)
+    Container(width: 200, height: 200, color: Colors.blue),
+    // العنصر الثاني (فوق الأول)
+    Container(width: 100, height: 100, color: Colors.red),
+    or 
+    Positioned(
+      top: 20, // المسافة من الأعلى
+      left: 30, // المسافة من اليسار
+      child: Container(width: 100, height: 100, color: Colors.red),
+    ),
+  ],
+)
+ */
+
+/*
+Scaffold(
+      backgroundColor: Color(0xFFE9FAFF),
+      body: SafeArea(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                alignment: Alignment.center,
+                // margin: EdgeInsets.only(bottom: 120),
+                width: 276,
+                height: 112,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/firstScreenAssets/logo.png'),
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 44),
+                width: 374,
+                height: 245,
+
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/firstScreenAssets/build.png'),
+                  ),
+                ),
+                foregroundDecoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/firstScreenAssets/motor.png'),
+                  ),
+                ),
+              ),
+            ),
+            // SizedBox(height: 20),
+            // Align(
+            //   child: Stack(
+            //     alignment:
+            //         Alignment.bottomCenter, // محاذاة العناصر في المنتصف يمين يسار
+            //     children: [
+            //       Positioned(
+            //         child: Image.asset('images/firstScreenAssets/build.png'),
+            //       ),
+
+            //       Positioned(
+            //         // top: 40,
+            //         child: Image.asset(
+            //           'images/firstScreenAssets/motor.png',
+            //           width: 270,
+            //           height: 209.27,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+            // Container(
+            //   alignment: Alignment.bottomCenter,
+            //   child: Column(
+            //     children: [
+
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+ */
+
+/*
+ // Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+
+      //     SizedBox(height: 120),
+      //   ],
+      // ),
+      // bottomSheet: Container(
+      //   alignment: Alignment.bottomCenter,
+      //   child: Stack(
+      //     children: [
+      //       Image.asset('images/firstScreenAssets/build.png'),
+      //       Positioned(
+      //         child: Image.asset('images/firstScreenAssets/motor.png'),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+ */
